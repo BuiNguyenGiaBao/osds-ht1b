300072ac93080733924679a5e4661b3392a8434c
Bùi Nguyễn Gia Bảo <162402329+BuiNguyenGiaBao@users.noreply.github.com>
2025-11-27 15:47:18 +0700

    1

--- 02-Thu thập dữ liệu với Selenium/BaiTap/bt6.py
+++ 02-Thu thập dữ liệu với Selenium/BaiTap/bt6.py
@@ -1,112 +1,113 @@
+from pygments.formatters.html import webify
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
 import pandas as pd
 import re
+import time
 
 # DataFrame kết quả
-d = pd.DataFrame({'name': [], 'birth': [], 'death': [], 'nationality': []})
+d = pd.DataFrame({
+    "name": [],
+    "birth": [],
+    "death": [],
+    "nationality": [],
+})
 
-# ------------ HÀM HỖ TRỢ: LẤY INFO TỪ TRANG 1 HOẠ SĨ -----------------
-def get_painter_info(driver, url):
-    driver.get(url)
-    time.sleep(1.5)
+all_links = []
 
-    # tên
+driver = webdriver.Chrome()
+
+try:
+    for i in range(70, 71):
+        url = f'https://en.wikipedia.org/wiki/List_of_painters_by_name_beginning_with_%22{chr(i)}%22'
+        driver.get(url)
+        time.sleep(5)
+
+        # Các <li> trong nội dung chính
+        li_tags = driver.find_elements(By.CSS_SELECTOR, "div.mw-parser-output ul li")
+
+        for li in li_tags:
+            try:
+                a = li.find_element(By.TAG_NAME, "a")
+                href = a.get_attribute("href")
+                # Chỉ lấy link bài wiki bình thường
+                if href and href.startswith("https://en.wikipedia.org/wiki/") and "List_of_" not in href:
+                    if href not in all_links:
+                        all_links.append(href)
+                        if len(all_links) >= 3:
+                            break
+            except:
+                pass
+
+        if len(all_links) >= 3:
+            break
+finally:
+    driver.quit()
+
+all_links = list(dict.fromkeys(all_links))
+print("Tổng số link painters (giới hạn):", len(all_links))
+
+
+for idx, link in enumerate(all_links, start=1):
+    print(idx, "/", len(all_links), ":", link)
+
+    driver = webdriver.Chrome()
     try:
-        name = driver.find_element(By.ID, "firstHeading").text
-    except:
-        name = ""
+        driver.get(link)
+        time.sleep(5)
 
-    # ngày sinh
-    try:
-        birth_element = driver.find_element(
-            By.XPATH, "//th[normalize-space()='Born']/following-sibling::td"
-        )
-        birth_text = birth_element.text
-        # lấy dạng "1 January 1900"
-        m = re.findall(r"[0-9]{1,2}\s+[A-Za-z]+\s+[0-9]{3,4}", birth_text)
-        birth = m[0] if m else birth_text.split("\n")[0]
-    except:
-        birth = ""
+        # name = title trang
+        try:
+            name = driver.find_element(By.TAG_NAME, "h1").text
+        except:
+            name = ""
 
-    # ngày mất
-    try:
-        death_element = driver.find_element(
-            By.XPATH, "//th[normalize-space()='Died']/following-sibling::td"
-        )
-        death_text = death_element.text
-        m = re.findall(r"[0-9]{1,2}\s+[A-Za-z]+\s+[0-9]{3,4}", death_text)
-        death = m[0] if m else death_text.split("\n")[0]
-    except:
-        death = ""
+        # birth
+        try:
+            birth_element = driver.find_element(
+                By.XPATH, "//th[text()='Born']/following-sibling::td"
+            )
+            birth_text = birth_element.text
+            # lấy dạng "1 January 1900"
+            m = re.findall(r"[0-9]{1,2}\s+[A-Za-z]+\s+[0-9]{4}", birth_text)
+            birth = m[0] if m else birth_text
+        except:
+            birth = ""
 
-    # quốc tịch
-    try:
-        nationality_element = driver.find_element(
-            By.XPATH, "//th[normalize-space()='Nationality']/following-sibling::td"
-        )
-        nationality = nationality_element.text.split("\n")[0]
-    except:
-        nationality = ""
+        # death
+        try:
+            death_element = driver.find_element(
+                By.XPATH, "//th[text()='Died']/following-sibling::td"
+            )
+            death_text = death_element.text
+            m = re.findall(r"[0-9]{1,2}\s+[A-Za-z]+\s+[0-9]{4}", death_text)
+            death = m[0] if m else death_text
+        except:
+            death = ""
 
-    return {
+        # nationality
+        try:
+            nationality_element = driver.find_element(
+                By.XPATH, "//th[text()='Nationality']/following-sibling::td"
+            )
+            nationality = nationality_element.text
+        except:
+            nationality = ""
+
+    finally:
+        driver.quit()
+
+    painter = {
         "name": name,
         "birth": birth,
         "death": death,
-        "nationality": nationality,
+        "nationality": nationality
     }
+d = pd.concat([d, pd.DataFrame([painter])], ignore_index=True)
 
-
-# ------------------ MAIN SCRAPER -----------------------
-driver = webdriver.Chrome()
-
-all_links = set()
-
-# vòng chữ cái A..Z (demo: A–C, muốn full thì Z + 1)
-for i in range(ord("A"), ord("C") + 1):
-    list_url = (
-        f'https://en.wikipedia.org/wiki/'
-        f'List_of_painters_by_name_beginning_with_%22{chr(i)}%22'
-    )
-    print("Đang lấy links ở:", list_url)
-    driver.get(list_url)
-    time.sleep(2)
-
-    # các <li> trong nội dung bài viết
-    li_tags = driver.find_elements(
-        By.CSS_SELECTOR, "div.mw-parser-output > ul > li"
-    )
-
-    for li in li_tags:
-        try:
-            a = li.find_element(By.TAG_NAME, "a")
-            href = a.get_attribute("href")
-            # chỉ lấy link wiki artist, bỏ các link list khác
-            if (
-                href
-                and href.startswith("https://en.wikipedia.org/wiki/")
-                and "List_of_painters_by_name_beginning_with" not in href
-                and ":" not in href  # bỏ các namespace đặc biệt
-            ):
-                all_links.add(href)
-        except:
-            pass
-
-print("Tổng số link hoạ sĩ lấy được:", len(all_links))
-
-# Lấy thông tin từng hoạ sĩ
-for idx, link in enumerate(list(all_links)):
-    if idx >= 50:      # giới hạn cho nhẹ, bỏ dòng này nếu muốn lấy hết
-        break
-    print(idx + 1, "/", len(all_links), "->", link)
-    try:
-        painter = get_painter_info(driver, link)
-        d = pd.concat([d, pd.DataFrame([painter])], ignore_index=True)
-    except Exception as e:
-        print("Lỗi với link:", link, e)
-
-driver.quit()
-
+# Xem kết quả
 print(d.head())
 
+# Lưu Excel
+d.to_excel("painters_limited.xlsx", index=False)
+print("Đã lưu file painters_limited.xlsx") 

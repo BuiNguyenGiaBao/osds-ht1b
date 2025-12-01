e6f216917be394a38ee7176a56b6fdaf1281ef33
Bùi Nguyễn Gia Bảo <162402329+BuiNguyenGiaBao@users.noreply.github.com>
2025-12-01 13:47:30 +0700

    a

--- 02-Thu thập dữ liệu với Selenium/BaiTap/bt_fox/ex03b.py
+++ 02-Thu thập dữ liệu với Selenium/BaiTap/bt_fox/ex03b.py
@@ -1,48 +1,34 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.firefox.service import Service         
+from selenium.webdriver.firefox.options import Options         
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
-from selenium.webdriver import ActionChains
 import time
-import pandas as pd
 
-# Đường dẫn đến file thực thi geckodriver
 gecko_path = r"D:/bt_fox/geckodriver.exe"
 
-# Khởi tởi đối tượng dịch vụ với đường geckodriver
-ser = Service(gecko_path)
+service = Service(executable_path=gecko_path)
 
-# Tạo tùy chọn
-options = webdriver.firefox.options.Options()
-options.binary_location ="C:/Program Files/Mozilla Firefox/firefox.exe"
-# Thiết lập firefox chỉ hiện thị giao diện
-options.headless = False
+options = Options()
+options.binary_location = r"C:/Program Files/Mozilla Firefox/firefox.exe"
+options.headless = False   # hiện giao diện
 
-# Khởi tạo driver
-driver = webdriver.Firefox(options = options, service=ser)
+driver = webdriver.Firefox(service=service, options=options)
 
-# Tạo url
-url = 'http://pythonscraping.com/pages/files/form.html'
-
-# Truy cập
+url = "http://pythonscraping.com/pages/files/form.html"
 driver.get(url)
 
-# Tạm dừng khoảng 2 giây
 time.sleep(2)
 
 firstname_input = driver.find_element(By.XPATH, "//input[@name='firstname']")
-lastname_input = driver.find_element(By.XPATH, "//input[@name='lastname']")
+lastname_input  = driver.find_element(By.XPATH, "//input[@name='lastname']")
 
-firstname_input.send_keys('Nhat Tung')
+firstname_input.send_keys("rondom")
 time.sleep(1)
-lastname_input.send_keys("Le")
+lastname_input.send_keys("hoo lee shee")
 
 time.sleep(2)
-buttton = driver.find_element(By.XPATH, "//input[@type='submit']")
-buttton.click()
-time.sleep(5)
+button = driver.find_element(By.XPATH, "//input[@type='submit']")
+button.click()
 
+time.sleep(5)
 driver.quit()
